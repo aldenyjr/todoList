bd678832f0f6d70498718b1caa220202e831f9b9
Aldeny Jr <114154072+aldenyjr@users.noreply.github.com>
2023-04-18 14:05:09 -0300

    adicionando estilos nas tarefas

--- src/components/Tarefas/index.js
+++ src/components/Tarefas/index.js
@@ -6,15 +6,13 @@ export default function Tarefas({ dados, onClick, onDelete }) {
     <div className={styles.itensContainer}>
       {dados.map((item) => (
         <div key={item.id} className={styles.itens}>
-          <p
-            className={item.checked ? styles.checked : ""}
-            onClick={() => onClick(item)}
-          >
-            {item.title}
-          </p>
+          <div onClick={() => onClick(item)}>
+            <p className={item.checked ? styles.checked : ""}>{item.title}</p>
+          </div>
           <div>
             {item.checked && <img src="./assets/icons/done.png" alt="iCON" />}
             <img
+              className={styles.iconeDelete}
               onClick={() => onDelete(item)}
               src="./assets/icons/del.png"
               alt="ICON"
